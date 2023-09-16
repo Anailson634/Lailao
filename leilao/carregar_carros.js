d677bd85530a34628cbac83fd02445c95da7ad71
Anailson634 <anonymoushack838@gmail.com>
2023-09-16 10:29:01 -0300

    Layout do 'leilao' aprimorado

--- leilao/carregar_carros.js
+++ leilao/carregar_carros.js
@@ -6,7 +6,7 @@ async function criar_carros() {
         headers: {
             "Content-type": "application/json"
         },
-        body: JSON.stringify({"quantidade": 2}) //Json que sera enviado
+        body: JSON.stringify({"quantidade": 20}) //Json que sera enviado
     }
     const url= await fetch("http://127.0.0.1:5000/gerar_carros", init) //carregando API, aplicando as cinfigurações
     dados_recebidos= await url.json() //Resposta recebida da API
@@ -15,12 +15,13 @@ async function criar_carros() {
 function carregar(){
     for (const chave in dados_recebidos) {
         let local_img=dados_recebidos[chave]['_path'].replace(/\/\/+/g, '/')
-        console.log(local_img)
+        
         var DivFilho=document.createElement("div")
         DivFilho.className="carros"
         DivFilho.id=chave
 
         DivFilho.style.background=`url(${local_img})`
+        DivFilho.style.backgroundSize='cover'
 
         DivPai.appendChild(DivFilho)
     }
